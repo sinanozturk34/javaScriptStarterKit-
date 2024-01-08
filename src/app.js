c2a057f8176f5ee44aa7f8aa7c914bdd8cbe17fe
Sinan Ozturk <sinansinan_@outlook.com.tr>
2024-01-08 12:25:08 +0300

    10.video

---
+++ src/app.js
@@ -0,0 +1,10 @@
+let sayi1=10;
+sayi1="Engin Demirog"
+let student={id:1,name:"Engin"}
+//console.log(student)
+
+function save(puan=10,ogrenci) {
+   console.log(ogrenci.name+" : "+puan);
+   
+}
+save(student);
